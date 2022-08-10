a7efac9a9d9086d173f877c1bd8f1d4b9dafa01c
Sneha <rockcenahhh100@gmail.com>
2022-08-10 14:22:14 -0700

    newly added app, index files

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Listbyevent from './component-test/Listbyevent';
+//import Qty from './Qty';
+function App(){
+  return(
+    <div>
+      <Listbyevent/>
+    </div>
+  );
+
+}
+export default App;
+
